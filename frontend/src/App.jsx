7a10c85c0af5da13fdb8b05ea1f075fb4021f7ef
Cole Tuininga <cole@tuininga.org>
2025-11-03 22:45:08 -0500

    Starting on class detail display.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ import StudentDetails from './student/StudentDetails';
 import StudentEdit from './student/StudentEdit';
 import StudentCreate from './student/StudentCreate';
 import ClassList from './class/ClassList';
+import ClassDetail from './class/ClassDetail';
 import Breadcrumbs from './Breadcrumbs';
 import './roundedCards.css';
 import './globalBackground.css';
@@ -122,6 +123,11 @@ function AppLayout() {
           <Route path="/classes" element={
             <PrivateRoute>
               <ClassList />
+            </PrivateRoute>
+          } />
+          <Route path="/classes/:id" element={
+            <PrivateRoute>
+              <ClassDetail />
             </PrivateRoute>
           } />
       </Routes>
