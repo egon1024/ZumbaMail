c585fe02c74626792072cf58d25f1fbbe964483d
Cole Tuininga <cole@tuininga.org>
2025-12-22 13:12:43 -0500

    Email blast basics working, new workflows

--- activity/routers/communication.py
+++ activity/routers/communication.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from activity.views import SessionEnrollmentCombinationsView, EmailDetailsView
+from activity.views import SessionEnrollmentCombinationsView, EmailDetailsView, EmailBlastView
 
 urlpatterns = [
     path('session-enrollments/', SessionEnrollmentCombinationsView.as_view(), name='session-enrollments'),
     path('email-details/<str:combination_id>/', EmailDetailsView.as_view(), name='email-details'),
+    path('email-blast/', EmailBlastView.as_view(), name='email-blast'),
 ]
